00c4cea9a156ab4b12a96895c66a7381c63e47c7
Jered Sweeney <sweeneyjered@gmail.com>
2018-01-11 02:48:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,11 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   configure do
-    enable :Sessions
+    enable :sessions
     set :session_secret, "secret"
   end
 
   get '/' do
     erb :index
+  end
+
+  post '/checkout' do
+
   end
 end
